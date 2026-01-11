4542b1c55dd73269d4ff9699cfb38616c7c62997
npakdeewong <nick.nitipon@gmail.com>
2026-01-11 16:40:07 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <img alt="Vue logo" src="../assets/Cat.jpg">
+        <h1>Contact to Nitipon Pakdeewong</h1>
+        <h3>Tel 086-370-4843</h3>
+
+    </div>
+</template>
+
+
+<style lang="">
+
+
+</style>
